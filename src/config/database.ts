36204c7403378f60b30c58c3c8fbce76bd552f65
Ayomide <ayomidekay7@gmail.com>
2026-07-08 14:25:58 +0100

    feat(aiven): update database file to include the SSL from the new AIVEN MySQL.

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,6 +10,9 @@ const db = Knex({
     user: env.db.user,
     password: env.db.password,
     database: env.db.name,
+    ssl: env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
   },
   pool: { min: 2, max: 10 },
 });
